914317c321d9856bcbd08057b51fb3429229afa3
Vladimir-cloudj <151816542+Dmitry-Baz@users.noreply.github.com>
2026-02-28 15:52:28 +0400

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/ra16-homeworks-router-menu/", //для локального запуска - закомментировать
+  base: "/ra16-homeworks-redux-editing/", //для локального запуска - закомментировать
 });
